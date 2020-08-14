939909b7cb896d057d9f31f88c501af4539d1097
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-08-15 01:44:28 +0200

    Update figf_BarMeanIndpndPlot1.m

--- figure/figf_BarMeanIndpndPlot1.m
+++ figure/figf_BarMeanIndpndPlot1.m
@@ -1,4 +1,4 @@
-function [ hs ] = figf_BarMeanIndpndPlot1( idVec, dataVec, cndtnVec, hax )
+function [ hs, hplt ] = figf_BarMeanIndpndPlot1( idVec, dataVec, cndtnVec, hax )
 %
 % Copyright (C) 2020 Yuichi Takeuchi
 
@@ -11,7 +11,7 @@ end
 tmpbarY = cellfun(@mean, cdata);
 
 barY = zeros(numel(tmpbarY));
-for i = 1:length(numel(tmpbarY))
+for i = 1:length(tmpbarY)
     barY(i,i) = tmpbarY(i);
 end
 
@@ -19,38 +19,43 @@ hold(hax,'on');
 hb = bar(hax, indX, barY, 0.5, 'stacked');
 
 unqIdVec = unique(idVec);
-
+hsplt = {};
 for i = 1:length(unqIdVec)
     idx = idVec == unqIdVec(i);
     xpi = cndtnVec(idx) + 0.25*(rand(size(cndtnVec(idx)))-0.5);
-    switch mod(i,12)
-        case 1
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--o');
-        case 2
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--+');
-        case 3
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--*');
-        case 4
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--x');
-        case 5 
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--s');
-        case 6
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--d');
-        case 7
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--^');
-        case 8
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--v');
-        case 9
-            hplt(i) = plot(hax, xpi, dataVec(idx), '-->');
-        case 10
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--<');
-        case 11
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--p');
-        case 12
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--h');
-        otherwise
-            hplt(i) = plot(hax, xpi, dataVec(idx), '--.');
-    end
+    xmat = reshape(xpi, length(unqX), []);
+    ymat = reshape(dataVec(idx), length(unqX), []);
+%     for j = 1:size(xmat,2)
+        switch mod(i,12)
+            case 1
+                hplt = plot(hax, xmat, ymat, '--o');
+            case 2
+                hplt = plot(hax, xmat, ymat, '--+');
+            case 3
+                hplt = plot(hax, xmat, ymat, '--*');
+            case 4
+                hplt = plot(hax, xmat, ymat, '--x');
+            case 5 
+                hplt = plot(hax, xmat, ymat, '--s');
+            case 6
+                hplt = plot(hax, xmat, ymat, '--d');
+            case 7
+                hplt = plot(hax, xmat, ymat, '--^');
+            case 8
+                hplt = plot(hax, xmat, ymat, '--v');
+            case 9
+                hplt = plot(hax, xmat, ymat, '-->');
+            case 10
+                hplt = plot(hax, xmat, ymat, '--<');
+            case 11
+                hplt = plot(hax, xmat, ymat, '--p');
+            case 12
+                hplt = plot(hax, xmat, ymat, '--h');
+            otherwise
+                hplt = plot(hax, xmat, ymat, '--.');
+        end
+        hsplt = [hsplt; hplt];
+%     end
 end
 
 hold(hax,'off');
@@ -62,7 +67,6 @@ htitle = title('');
 % Building the output handle structure
 hs.ax = hax;
 hs.bar = hb;
-hs.plt = hplt;
 hs.ylbl = hylabel;
 hs.xlbl = hxlabel;
 hs.ttl = htitle;
