239fcd263a495418d98a8212f7ce50b1eb2bda8d
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-08-27 16:12:20 +0200

    update stats

--- statistics/sem.m
+++ statistics/sem.m
@@ -1,3 +1,3 @@
 function y = sem(x)
-y = std(x,'omitnan') / sqrt(size(x,2));
+y = std(x,'omitnan') / sqrt(size(x,1));
 end
--- statistics/stats_Summary3.m
+++ statistics/stats_Summary3.m
@@ -15,12 +15,12 @@ Std = zeros(1, 2);
 Sem = zeros(1, 2);
 Prctile = zeros(7, 2);
 
-Mean(1) = mean(data1);
-Std(1) = std(data1);
+Mean(1) = mean(data1, 'omitnan');
+Std(1) = std(data1, 'omitnan');
 Sem(1) = sem(data1);
 Prctile(:,1) = prctile(data1, [0 10 25 50 75 90 100]);
 
-Mean(2) = mean(data2);
-Std(2) = std(data2);
+Mean(2) = mean(data2, 'omitnan');
+Std(2) = std(data2, 'omitnan');
 Sem(2) = sem(data2);
 Prctile(:,2) = prctile(data2, [0 10 25 50 75 90 100]);
