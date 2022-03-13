f0259a7e0f57a5c6d47717755efad58a554aa42f
ytake-lab <ytake@pharm.hokudai.ac.jp>
2022-03-13 20:46:58 +0900

    Update plotf_ScaledTraces1.m

--- figure/plotf_ScaledTraces1.m
+++ figure/plotf_ScaledTraces1.m
@@ -1,13 +1,12 @@
-function [ hp ] = plotf_ScaledTraces1(hax, t, data, ChGroup)
+function [hp]=plotf_ScaledTraces1(hax,t,data,ChGroup)
 %
-% Copyright (C) Yuichi Takeuchi 2018
+% Copyright (C) 2018-2022 Yuichi Takeuchi
 
 % axis off
 hold(hax,'on')
-for i = 1:numel(ChGroup)
-    srcWave = data(ChGroup(i),:);
-    srcWave = srcWave + 2*i - numel(ChGroup);
-    hp = plot(t, srcWave, 'k', 'LineWidth', 0.1);
+for i=1:numel(ChGroup)
+    srcWave=data(ChGroup(i),:);
+    srcWave=srcWave+2*i-numel(ChGroup)-1;
+    hp{i}=plot(t,srcWave,'k','LineWidth', 0.1);
 end
 hold(hax,'off')
-
